e1164569f412ef2ba65de5ce12e87fc80edc56de
Jason Bryer <jason@bryer.org>
2014-08-31 17:22:20 -0400

    Added analysis using NAPCS ratings instead of rankings.

--- R/State Rankings.R
+++ R/State Rankings.R
@@ -25,6 +25,46 @@ g4read$g4read <- 1:nrow(g4read)
 g8math$g8math <- 1:nrow(g8math)
 g8read$g8read <- 1:nrow(g8read)
 
+##### For comparing scores #####################################################
+scores <- merge(rankings, g4math[,c('level2','diffwtd')], by.x='abbr', by.y='level2', all.x=TRUE)
+names(scores)[ncol(scores)] <- 'g4math'
+scores <- merge(scores, g4read[,c('level2','diffwtd')], by.x='abbr', by.y='level2', all.x=TRUE)
+names(scores)[ncol(scores)] <- 'g4read'
+scores <- merge(scores, g8math[,c('level2','diffwtd')], by.x='abbr', by.y='level2', all.x=TRUE)
+names(scores)[ncol(scores)] <- 'g8math'
+scores <- merge(scores, g8read[,c('level2','diffwtd')], by.x='abbr', by.y='level2', all.x=TRUE)
+names(scores)[ncol(scores)] <- 'g8read'
+
+df.melted <- melt(scores[,c('abbr','NAPCS2010Score','g4math','g4read','g8math','g8read')],
+				  id=c('abbr','NAPCS2010Score'))
+df.melted$variable <- factor(df.melted$variable, levels=c('g4math','g4read','g8math','g8read'),
+							 labels=c('Grade 4 Math', 'Grade 4 Reading',
+							 		  'Grade 8 Math', 'Grade 8 Reading'))
+df.cor <- data.frame(variable=c('g4math','g4read','g8math','g8read'),
+					 cor=c(cor(scores$NAPCS2010Score, scores$g4math, use='pairwise.complete.obs'),
+					 	   cor(scores$NAPCS2010Score, scores$g4read, use='pairwise.complete.obs'),
+					 	   cor(scores$NAPCS2010Score, scores$g8math, use='pairwise.complete.obs'),
+					 	   cor(scores$NAPCS2010Score, scores$g8read, use='pairwise.complete.obs')),
+					 stringsAsFactors=FALSE)
+df.cor$variable <- factor(df.cor$variable, levels=c('g4math','g4read','g8math','g8read'),
+							 labels=c('Grade 4 Math', 'Grade 4 Reading',
+							 		 'Grade 8 Math', 'Grade 8 Reading'))
+df.cor$x <- 40
+df.cor$y <- 15
+
+ggplot(df.melted, aes(x=NAPCS2010Score, y=value, label=abbr)) +
+	geom_point() +
+	geom_text(size=2.5, vjust=-1) +
+ 	geom_text(data=df.cor, aes(x=x, y=y, 
+ 			  label=paste0('Correlation = ', round(cor, digits=3))),
+ 			  hjust=0, size=5, parse=FALSE) +
+	facet_wrap(~ variable) +
+	xlab('NAPCS Score for Quality of State Charter Law (out of a maximum score of 208)') +
+	ylab('NAEP Standardized Mean Difference (Effect Size)')
+
+ggsave('../Figures2009/LawScoresVsNAEPDifferences.pdf', width=12, height=9)
+
+##### For comparing rankings ###################################################
 rankings <- merge(rankings, g4math[,c('level2','g4math')], by.x='abbr', by.y='level2', all.x=TRUE)
 rankings <- merge(rankings, g4read[,c('level2','g4read')], by.x='abbr', by.y='level2', all.x=TRUE)
 rankings <- merge(rankings, g8math[,c('level2','g8math')], by.x='abbr', by.y='level2', all.x=TRUE)
@@ -40,7 +80,8 @@ p.g4math <- ggplot(df.melted,aes(x=variable,y=value)) +
 	geom_text(aes(label=abs(value)),size=2.5) +
 	scale_y_continuous(name="", breaks=yvals$value, labels=yvals$State) +
 	theme_slopegraph()
-# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', method='spaced', min.space=0.05)
+# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', 
+# 					   method='spaced', min.space=0.05)
 # df <- df[df$y != 0,]
 # p.g4math <- plot_slopegraph(df)
 
@@ -54,7 +95,8 @@ p.g4read <- ggplot(df.melted,aes(x=variable,y=value)) +
 	geom_text(aes(label=abs(value)),size=2.5) +
 	scale_y_continuous(name="", breaks=yvals$value, labels=yvals$State) +
 	theme_slopegraph()
-# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', method='spaced', min.space=0.05)
+# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', 
+# 					   method='spaced', min.space=0.05)
 # df <- df[df$y != 0,]
 # p.g4read <- plot_slopegraph(df)
 
@@ -68,7 +110,8 @@ p.g8math <- ggplot(df.melted,aes(x=variable,y=value)) +
 	geom_text(aes(label=abs(value)),size=2.5) +
 	scale_y_continuous(name="", breaks=yvals$value, labels=yvals$State) +
 	theme_slopegraph()
-# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', method='spaced', min.space=0.05)
+# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', 
+# 					   method='spaced', min.space=0.05)
 # df <- df[df$y != 0,]
 # p.g8math <- plot_slopegraph(df)
 
@@ -82,7 +125,8 @@ p.g8read <- ggplot(df.melted,aes(x=variable,y=value)) +
 	geom_text(aes(label=abs(value)),size=2.5) +
 	scale_y_continuous(name="", breaks=yvals$value, labels=yvals$State) +
 	theme_slopegraph()
-# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', method='spaced', min.space=0.05)
+# df <- build_slopegraph(df.melted, x='variable', y='value', group='State', 
+# 					   method='spaced', min.space=0.05)
 # df <- df[df$y != 0,]
 # p.g8read <-	plot_slopegraph(df)
 
